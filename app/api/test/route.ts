76359d86c664d93d9bd417c656566c3ab1437fe5
hyobin <hbaloha@naver.com>
2025-01-21 16:21:38 +0900

    Error Handling

--- app/api/test/route.ts
+++ app/api/test/route.ts
@@ -6,6 +6,5 @@ export async function GET(request: Request) {
   const randomDelay = Math.floor(Math.random() * 5000);
   console.log('randomDelay', randomDelay);
 
-  await delay(randomDelay);
-  return NextResponse.json({ data: `${randomDelay}만에 응답완료!` });
+  throw new Error();
 }
